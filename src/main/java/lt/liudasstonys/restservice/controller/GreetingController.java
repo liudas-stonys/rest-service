d4bdf4e1bbec94034b96eb4e913c9d04129b90ff
Liudas Stonys <liudas.stonys@x.insoft.lt>
2019-10-29 16:07:31 +0200

    tuesday work commit

--- src/main/java/lt/liudasstonys/restservice/controller/GreetingController.java
+++ src/main/java/lt/liudasstonys/restservice/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package lt.liudasstonys.restservice.controller;
 
 import lt.liudasstonys.restservice.entity.Greeting;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,14 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//    @RequestMapping("/greeting")
+//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        return new Greeting(counter.incrementAndGet(),
+//                String.format(template, name));
+//    }
+
+    @RequestMapping("/greeting/{name}")
+    public Greeting greeting(@PathVariable(value="name") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
--- src/main/java/lt/liudasstonys/restservice/entity/Greeting.java
+++ src/main/java/lt/liudasstonys/restservice/entity/Greeting.java
@@ -1,20 +1,28 @@
 package lt.liudasstonys.restservice.entity;
 
+import lombok.*;
+
+@Data
+//@NoArgsConstructor
+//@RequiredArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
 public class Greeting {
 
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }
+//    public Greeting(long id, String content) {
+//        this.id = id;
+//        this.content = content;
+//    }
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
 }
